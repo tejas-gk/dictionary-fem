bbebbf2f5846ddf961f8e82df76099c2fdc15366
tejas-gk <tejgankri@outlook.com>
2023-05-10 17:05:23 +0530

    fix: text center on large screens

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -5,7 +5,7 @@ export default function NoResults() {
   const [font] = useContext(FontContext);
 
   return (
-    <div className={`${font.font} w-screen lg:w-none`}>
+    <div className={`${font.font}`}>
       <h1 className='lg:text-7xl text-xl font-extrabold flex items-center justify-center mt-9'>
         No results found
       </h1>
